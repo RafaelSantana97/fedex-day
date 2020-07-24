5bf72ccb1fdda37000e3d41eef20dbb009a0f0cf
Rafael Santana <rafaellauton@outlook.com>
2020-07-24 13:12:03 -0300

    respawn time balanced

--- Tutorial/Assets/Scripts/Mechanics/EnemyCorona.cs
+++ Tutorial/Assets/Scripts/Mechanics/EnemyCorona.cs
@@ -10,20 +10,6 @@ namespace Platformer.Mechanics
     [RequireComponent(typeof(Collider2D))]
     public class EnemyCorona : MonoBehaviour 
     {
-
-        [SerializeField] private float timeBeforeDisapear = 3f;
-
-        void Start()
-        {
-            StartCoroutine(WaitAndDisapear());
-        }
-
-        private IEnumerator WaitAndDisapear()
-        {
-            yield return new WaitForSeconds(timeBeforeDisapear);
-            Destroy(gameObject);
-        }
-
         void OnCollisionEnter2D(Collision2D collision)
         {
             var player = collision.gameObject.GetComponent<PlayerController>(); 
---
+++ Tutorial/Assets/Scripts/Mechanics/EnemyCoronaDespawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+
+    public class EnemyCoronaDespawn : MonoBehaviour 
+    {
+
+        [SerializeField] private float timeBeforeDisapearMin = 2.5f;
+        [SerializeField] private float timeBeforeDisapearMax = 4f;
+
+        void Start()
+        {
+            StartCoroutine(WaitAndDisapear());
+        }
+
+        private IEnumerator WaitAndDisapear()
+        {
+            yield return new WaitForSeconds(Random.Range(timeBeforeDisapearMin, timeBeforeDisapearMax));
+            Destroy(gameObject);
+        }
+    }
+}
