94026d75cf0885bb1ed153659124c7992bc25d43
Henry Smith <henrysmith@users.noreply.github.com>
2020-07-24 03:19:08 -0400

    adding a banana obstacle

---
+++ Tutorial/Assets/Scripts/Mechanics/ObstacleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+
+    [RequireComponent(typeof(Collider2D))]
+    public class ObstacleController : MonoBehaviour
+    {
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            //only exectue OnPlayerEnter if the player collides with this token.
+            var player = other.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                Schedule<PlayerHealthDecrease>();
+            }
+        }
+    }
+}
